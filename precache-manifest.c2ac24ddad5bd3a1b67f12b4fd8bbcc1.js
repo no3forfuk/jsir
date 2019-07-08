ff48f17d68607938fe7e782e98b2d07ec74a0b5b
jsir <no3forfuk@163.com>
2019-07-08 21:47:09 +0800

    deploy

---
+++ precache-manifest.c2ac24ddad5bd3a1b67f12b4fd8bbcc1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/jsir/robots.txt"
+  },
+  {
+    "revision": "ce6cc851b759ce59c204",
+    "url": "/jsir/js/chunk-vendors.2d385647.js"
+  },
+  {
+    "revision": "deb69425afe9041b1a4c",
+    "url": "/jsir/js/app.55298b18.js"
+  },
+  {
+    "revision": "521a5fb46f0438823cbc",
+    "url": "/jsir/js/about.6130274d.js"
+  },
+  {
+    "revision": "4a7da7b571451abe8456aeed04521f15",
+    "url": "/jsir/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/jsir/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a33d50da063b016852d1d139cf6e73b1",
+    "url": "/jsir/img/Water_1_M_Normal.a33d50da.jpg"
+  },
+  {
+    "revision": "deb69425afe9041b1a4c",
+    "url": "/jsir/css/app.b2570d08.css"
+  }
+];
